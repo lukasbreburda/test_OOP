ca49a50f0e0dd0a733df6fa11ba73bd4bbce9fdc
Lukáš Breburda <breburda.dolakova@gmail.com>
2016-12-07 18:07:13 +0100

    Test_OOP

---
+++ text_OOP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Seznam_aut> Auta =  new List<Seznam_aut>();
+            for (int r = 0; r<5; r++)
+            {
+                Seznam_aut auto = new Seznam_aut();
+                auto.color = "red" +r;
+                auto.tachometr = 183563+r;
+                auto.rok = 2000;
+                auto.stav = true;
+                Auta.Add(auto); 
+            }
+            int v = 0;
+            string jede;
+            int tach = 0;
+            string oznaceni = "";
+            foreach(var Value in Auta)
+            {
+                if (Value.stav == true)
+                {
+                    jede = "V provozu";
+                }
+                else
+                {
+                    jede = "Mimo provoz";
+                }
+                if (tach < Value.tachometr)
+                {
+                    tach = Value.tachometr;
+                }
+
+                
+                v++;
+                Console.WriteLine(v + ".  Auto barvy: " + Value.color + " Stav tachometru: " + Value.tachometr + " Rok výroby: " + Value.rok + " Stav: " + jede + "  " + oznaceni);
+                Console.WriteLine();
+                
+            }
+            Console.WriteLine("Nejvíce ujelo auto ze stavem tachometru: " + tach.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ text_OOP/Seznam_aut.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_OOP
+{
+    class Seznam_aut
+    {
+        private string _color;
+        private int _tachometr;
+        private int _rok;
+        private bool _stav;
+
+        public string color
+        {
+         get { return _color; }
+         set { _color = value; }
+        }
+        public int tachometr
+        {
+            get { return _tachometr; }
+            set { _tachometr = value; }
+        }
+        public int rok
+        {
+            get { return _rok; }
+            set { _rok = value; }
+        }
+        public bool stav
+        {
+            get { return _stav; }
+            set { _stav = value; }
+        }
+    }
+}
